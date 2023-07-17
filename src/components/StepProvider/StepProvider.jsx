23257cbc67ef3af599551343ee9e6a17f0dfe062
Dave Henson <dave.henson@gmail.com>
2023-07-17 20:48:01 +0100

    🐛 FIX:  initial step

--- src/components/StepProvider/StepProvider.jsx
+++ src/components/StepProvider/StepProvider.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const StepContext = React.createContext();
 
 function StepProvider({ children }) {
-  const [step, setStep] = React.useState(4);
+  const [step, setStep] = React.useState(1);
 
   function incrementStep(event) {
     event.preventDefault();
